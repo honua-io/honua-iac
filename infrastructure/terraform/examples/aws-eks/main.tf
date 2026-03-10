89d442fd437b76f61eb1b01d4ad74468e051a0fb
makani <mike@honua.io>
2026-03-09 23:04:28 -1000

    Format EKS example for validation

--- infrastructure/terraform/examples/aws-eks/main.tf
+++ infrastructure/terraform/examples/aws-eks/main.tf
@@ -5,18 +5,18 @@ provider "aws" {
 module "eks" {
   source = "../../modules/aws-eks"
 
-  name_prefix          = var.name_prefix
-  environment          = var.environment
-  tags                 = var.tags
-  vpc_cidr             = var.vpc_cidr
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
-  cluster_version      = var.cluster_version
-  node_instance_types  = var.node_instance_types
+  name_prefix           = var.name_prefix
+  environment           = var.environment
+  tags                  = var.tags
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  cluster_version       = var.cluster_version
+  node_instance_types   = var.node_instance_types
   node_cpu_architecture = var.node_cpu_architecture
-  node_min_size        = var.node_min_size
-  node_max_size        = var.node_max_size
-  node_desired_size    = var.node_desired_size
+  node_min_size         = var.node_min_size
+  node_max_size         = var.node_max_size
+  node_desired_size     = var.node_desired_size
 }
 
 output "cluster_name" {
