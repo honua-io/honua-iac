e97fd7e01f21d3e5c0cb8bcaf167a30a9470406f
Mike McDougall <mike@honua.io>
2026-09-08 01:24:01 -1000

    style: terraform fmt

--- infrastructure/terraform/examples/aws-cert/lambda-preview-cert.tf
+++ infrastructure/terraform/examples/aws-cert/lambda-preview-cert.tf
@@ -248,7 +248,7 @@ data "aws_iam_policy_document" "lambda_preview_certification" {
   }
 
   statement {
-    sid       = "InvokeAndDeleteTaggedCertificationFunction"
+    sid = "InvokeAndDeleteTaggedCertificationFunction"
     # UpdateFunctionConfiguration: the lane writes an inert environment nonce
     # on the per-run function before the cold-start evidence invoke
     # (honua-server#4548); certification run 20 was denied it on
